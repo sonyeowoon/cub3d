fec9f786fb670b738c3a40dd5d55f6eeeef9b146
Jeongin Choi <jechoi@c1r3s5.42gyeongsan.kr>
2025-11-13 15:39:07 +0900

    parse edit

--- src/parse/parse_free.c
+++ src/parse/parse_free.c
@@ -6,11 +6,23 @@
 /*   By: jechoi <jechoi@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:21:38 by jechoi            #+#    #+#             */
-/*   Updated: 2025/11/13 14:40:32 by jechoi           ###   ########.fr       */
+/*   Updated: 2025/11/13 15:38:54 by jechoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	free_asset(t_assets *asset)
+{
+	if (asset->north.path)
+		free(asset->north.path);
+	if (asset->south.path)
+		free(asset->south.path);
+	if (asset->east.path)
+		free(asset->east.path);
+	if (asset->west.path)
+		free(asset->west.path);
+}
 
 void	parse_free_data(t_data *data)
 {
@@ -26,12 +38,9 @@ void	parse_free_data(t_data *data)
 	if (data->file.file)
 	{
 		i = -1;
-		while (data->file.file[i])
+		while (data->file.file[++i])
 			free(data->file.file[i]);
 		free(data->file.file);
 	}
-	free(data->asset.north.path);
-	free(data->asset.south.path);
-	free(data->asset.east.path);
-	free(data->asset.west.path);
+	free_asset(&data->asset);
 }
